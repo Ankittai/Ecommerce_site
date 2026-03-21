f36ea2d0af115c8ad988c0b2fc68c4641ddc76b3
Ankit Kumar Singh <ankit@Ankits-MacBook-Air.local>
2026-03-21 19:20:48 +0530

    made changes to pre-populate slug field

--- myapp/models.py
+++ myapp/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 
+#suppose in name of product name there is space so this slugigy
+#will replace space with any char
+from django.utils.text import slugify
+
 # Create your models here.
 class Product(models.Model):
     name=models.CharField(max_length=100)
@@ -7,6 +11,12 @@ class Product(models.Model):
     description=models.TextField()
     image=models.ImageField(upload_to='images/')#install pillow for image field
     #“SlugField stores a URL-friendly version of a string, typically used to create readable and SEO-friendly URLs.”
-    slug=models.SlugField(max_length=100)
+    slug=models.SlugField(unique=True,blank=True)
     stock=models.IntegerField()
     active=models.BooleanField()
+
+    def save(self,*args,**kwargs):
+        if not self.slug:
+            self.slug=slugify(self.name)
+
+        super().save(*args,**kwargs)
