95af9c3c2e156da7c9b1fb0e3e799c3dfc39b0db
Ankit Kumar Singh <ankit@Ankits-MacBook-Air.local>
2026-03-21 18:27:10 +0530

    created model name Product

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    name=models.CharField(max_length=100)
+    price=models.FloatField()
+    description=models.TextField()
+    image=models.ImageField(upload_to='images/')#install pillow for image field
+    #“SlugField stores a URL-friendly version of a string, typically used to create readable and SEO-friendly URLs.”
+    slug=models.SlugField(max_length=100)
+    stock=models.IntegerField()
+    active=models.BooleanField()
